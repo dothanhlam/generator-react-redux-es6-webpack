0827a9e4cd913f8775984bb6e18920f68a678e72
Lam Do <dothanhlam@gmail.com>
2016-05-12 09:02:33 +0700

    test component template

--- generators/component/templates/component.js
+++ generators/component/templates/component.js
@@ -1,7 +1,16 @@
 import React, {Component,PropTypes} from 'react';
 
-class <%= name % >extends Component {
-    render() {
+class <%= name %>extends Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+    static displayName = "<%= name %>";
+    static propTypes = {};
 
+    buildComponent(props, state) {
+        return (<div><%=name%></div>);
+    }
+    render() {
+        return this.buildComponent(this.props, this.state)
     }
 }
